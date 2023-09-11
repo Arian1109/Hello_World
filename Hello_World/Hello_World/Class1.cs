0aab5607117c7019295b5df5fc8608d8f3702456
Arian1109 <144732110+Arian1109@users.noreply.github.com>
2023-09-11 22:03:55 +0200

    Add files via upload
    Es speichert die Daten und gibt sie wieder heraus.

---
+++ Hello_World/Hello_World/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+             
+            
+            Console.Write("Geben Sie Ihren Vornamen ein: ");
+            string vorname = Console.ReadLine();
+
+            
+            Console.Write("Geben Sie Ihren Nachnamen ein: ");
+            string nachname = Console.ReadLine();
+
+            
+            Console.Write("Geben Sie Ihr Alter ein: ");
+            int alter = Convert.ToInt32(Console.ReadLine());
+
+            
+            Console.WriteLine("Hallo " + vorname + " " + nachname + "!");
+            Console.WriteLine("Ihr Alter beträgt " + alter + " Jahre.");
+        }
+    }
+}
+
+
